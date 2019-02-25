645234df5e069a3b91a52ced7871204a2bffae34
Jeongmin Kim <jeongnna@Jeongmins-MBP.lan>
2019-02-25 17:14:12 +0900

    update

--- scripts/01_portfolio_analysis.R
+++ scripts/01_portfolio_analysis.R
@@ -2,29 +2,29 @@ library(tidyverse)
 library(lubridate)
 library(clValid)
 library(quadprog)
-library(parallel)
+library(pbmcapply)
 source("src/data_transformation.R")
 source("src/clustering_utils.R")
 source("src/portfolio_utils.R")
-# source("src/cgarch_roll.R")
+source("src/cgarch_roll.R")
 first <- dplyr::first
 last <- dplyr::last
 
 
 # Arguments ---------------------------------------------------------------
 
-# mc_cores <- 1
-# analysis_year <- 2011  # 2011 ~ 2018
 for (arg in commandArgs(TRUE)) {
   eval(parse(text=arg))
 }
 
+mc_cores <- 6
+
 # case grid
 grd <- expand_grid(
-  period_length = c(400, 800, 1600),
-  clmethod = c("hclust_pearson", "hclust_kendall"),
-  covmethod = c("sample"),
-  optim = c("gmv")
+  period_length = c(1600),
+  clmethod = c("hclust_pearson"),
+  covmethod = c("garch"),
+  optim = c("tangency")
 )
 
 # number of clusters
@@ -37,27 +37,44 @@ ncmax <- 6
 data_dir <- "data/processed/"
 daily <- read_csv(str_c(data_dir, "daily.csv"))
 market_cap <- read_csv(str_c(data_dir, "market_cap.csv"))
+risk_free <- read_csv(str_c(data_dir, "cd_rate.csv"))
 
 daily <- 
+  daily %>% 
+  left_join(risk_free, by = "date") %>% 
+  mutate(year = year(date), month = month(date)) %>% 
   left_join(
-    daily %>% 
-      mutate(year = year(date), month = month(date)),
-    market_cap %>% 
-      transmute(code, year = year(date), month = month(date), size),
+    market_cap %>% transmute(code, year = year(date), month = month(date), size),
     by = c("code", "year", "month")
   ) %>% 
-  select(code, date, logret, size)
+  select(code, date, logret, rf, size)
 
+
+###########################################################################
+###########################################################################
+for (analysis_year in 2015:2018) {
+  cat(
+    "\n",
+    "analysis year: ", analysis_year,
+    "\n",
+    sep = ""
+  )
 
 # Get all portfolio returns -----------------------------------------------
 
 date_set <- unique(daily$date)  # length: 3704
 start <- first(which(date_set >= str_c(analysis_year, "-01-01")))
-end <- last(which(date_set >= str_c(analysis_year, "-12-31")))
+end <- last(which(date_set <= str_c(analysis_year, "-12-31")))
 analysis_period <- date_set[start:end]
-risk_free <- NULL
 
 process <- function(len, clmethod, covmethod, optim, mc_cores) {
+  cat(
+    "len: ", len,
+    ", clmethod: ", clmethod,
+    ", covmethod: ", covmethod,
+    ", optim: ", optim,
+    "\n", sep = ""
+  )
   c_ctrl <- list(
     ncmin = 4,
     ncmax = 6,
@@ -65,13 +82,12 @@ process <- function(len, clmethod, covmethod, optim, mc_cores) {
   )
   p_ctrl <- list(
     optim = optim,
-    covmethod = covmethod,
-    risk_free = risk_free
+    covmethod = covmethod
   )
   get_portfolio_return(daily, analysis_period, len, c_ctrl, p_ctrl, mc_cores)
 }
 
-process_time <- system.time({
+system.time({
   pf_return_list <- mapply(
     FUN = process,
     len = grd$period_length,
@@ -93,5 +109,9 @@ allpofol_returns <-
   setNames(c("date", "avg", case_names))
 
 # save result
-out_path <- str_c("outputs/portfolio_returns/", analysis_year, ".RData")
+out_path <- str_c("outputs/garch-tan-", analysis_year, ".RData")
 save(allpofol_returns, file = out_path)
+
+}
+###########################################################################
+###########################################################################
--- scripts/test.R
+++ scripts/test.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(lubridate)
 library(clValid)
 library(quadprog)
-library(parallel)
+library(pbmcapply)
 source("src/data_transformation.R")
 source("src/clustering_utils.R")
 source("src/portfolio_utils.R")
-# source("src/cgarch_roll.R")
+source("src/cgarch_roll.R")
 first <- dplyr::first
 last <- dplyr::last
 
@@ -17,12 +17,14 @@ for (arg in commandArgs(TRUE)) {
   eval(parse(text=arg))
 }
 
+mc_cores <- 2
+
 # case grid
 grd <- expand_grid(
   period_length = c(100),
-  clmethod = c("hclust_pearson", "hclust_kendall"),
+  clmethod = c("hclust_pearson"),
   covmethod = c("sample"),
-  optim = c("gmv")
+  optim = c("gmv", "tangency")
 )
 
 # number of clusters
@@ -35,27 +37,34 @@ ncmax <- 6
 data_dir <- "data/processed/"
 daily <- read_csv(str_c(data_dir, "daily.csv"))
 market_cap <- read_csv(str_c(data_dir, "market_cap.csv"))
+risk_free <- read_csv(str_c(data_dir, "cd_rate.csv"))
 
 daily <- 
+  daily %>% 
+  left_join(risk_free, by = "date") %>% 
+  mutate(year = year(date), month = month(date)) %>% 
   left_join(
-    daily %>% 
-      mutate(year = year(date), month = month(date)),
-    market_cap %>% 
-      transmute(code, year = year(date), month = month(date), size),
+    market_cap %>% transmute(code, year = year(date), month = month(date), size),
     by = c("code", "year", "month")
   ) %>% 
-  select(code, date, logret, size)
-
+  select(code, date, logret, rf, size)
+  
 
 # Get all portfolio returns -----------------------------------------------
 
 date_set <- unique(daily$date)  # length: 3704
 start <- first(which(date_set >= str_c(analysis_year, "-01-01")))
-end <- start + 19
+end <- start + 9
 analysis_period <- date_set[start:end]
-risk_free <- NULL
 
 process <- function(len, clmethod, covmethod, optim, mc_cores) {
+  cat(
+    "len: ", len,
+    ", clmethod: ", clmethod,
+    ", covmethod: ", covmethod,
+    ", optim: ", optim,
+    "\n", sep = ""
+  )
   c_ctrl <- list(
     ncmin = 4,
     ncmax = 6,
@@ -63,13 +72,12 @@ process <- function(len, clmethod, covmethod, optim, mc_cores) {
   )
   p_ctrl <- list(
     optim = optim,
-    covmethod = covmethod,
-    risk_free = risk_free
+    covmethod = covmethod
   )
   get_portfolio_return(daily, analysis_period, len, c_ctrl, p_ctrl, mc_cores)
 }
 
-process_time <- system.time({
+system.time({
   pf_return_list <- mapply(
     FUN = process,
     len = grd$period_length,
@@ -91,11 +99,11 @@ allpofol_returns <-
   setNames(c("date", "avg", case_names))
 
 # test
-load("outputs/portfolio_returns/testing.RData")
+load("outputs/test.RData")
 cat("\n")
-cat(all(signif(allpofol_returns[[2]], 3) == signif(correct_allpofol_returns[[2]], 3)))
+cat(all(signif(allpofol_returns[[2]], 3) == signif(test[[2]], 3)))
 cat("\n")
-cat(all(signif(allpofol_returns[[3]], 3) == signif(correct_allpofol_returns[[3]], 3)))
+cat(all(signif(allpofol_returns[[3]], 3) == signif(test[[3]], 3)))
 cat("\n")
-cat(all(signif(allpofol_returns[[4]], 3) == signif(correct_allpofol_returns[[4]], 3)))
+cat(all(signif(allpofol_returns[[4]], 3) == signif(test[[4]], 3)))
 cat("\n\n")
